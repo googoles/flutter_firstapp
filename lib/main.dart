5dd212efa02eb1a6f68cc5c7f963d20a62a1882c
yunsoowow64 <46067507+googoles@users.noreply.github.com>
2020-01-23 17:00:07 +0900

    In Progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold( // General Layout
-    appBar: AppBar(
-      title: Text('My FIrst App'),
-      centerTitle: true,
-      backgroundColor: const Color(0XFF332F43),
-    ),
-    body: Center(
-      child: Text("Hello World"),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-    ),
-  ),
+  home: Home(),
+
 ));
 
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My FIrst App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[300],
+      ),
+      body: Center(
+        child: Text(
+          'Hello worlds',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[500],
+            fontFamily: 'SourceSansPro-Regular',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+
+  }}
