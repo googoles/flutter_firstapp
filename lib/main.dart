e9d0ffc09dc535914e86c50e34ec903786641e4b
yunsoowow64 <46067507+googoles@users.noreply.github.com>
2020-01-23 16:18:50 +0900

    In Progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
+  home: Scaffold( // General Layout
     appBar: AppBar(
       title: Text('My FIrst App'),
+      centerTitle: true,
+      backgroundColor: const Color(0XFF332F43),
     ),
-  )
+    body: Center(
+      child: Text("Hello World"),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('click'),
+    ),
+  ),
 ));
 
